8c4d906e3258ed4431886832355bab261edbbb42
LynnAmsbury <Lynn8542@gmail.com>
2020-05-17 13:47:30 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  # We want to return the languages as keys in new_hash and type/style as keys in the language subhash.
+  new_hash = {}
+  languages.each do |style, language| # language is a HASH currently
+    language.each do |lang, type_value| # type is also a HASH currently
+      type_value.each do |type_key, value|
+        if new_hash[lang] == nil
+          new_hash[lang] = { :type => value, :style => [style] }
+        else
+          new_hash[lang][:style] << style
+        end
+      end
+    end
+  end
+  new_hash
 end
